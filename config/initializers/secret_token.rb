9bdc6f06e41317b8bae07175147e2bebd563dc3b
Vivek Lath <techventures@intellexmarketing.com>
2012-11-14 12:44:17 +0530

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+IndianContentMarket::Application.config.secret_token = '9a9d427e821551fd39c9aa7ce48d5a070a5a1f354ade5c2c163410e9266dea46c4bc64ca70d573985182c9fc94a5c981a27acc4c44ff6f3ba600d471cdeb9146'
